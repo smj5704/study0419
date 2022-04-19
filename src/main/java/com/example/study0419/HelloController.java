ea333246ea2131ebbc018f532769a4b9c4d9af63
SAMSUNG PC <smj5704@naver.com>
2022-04-19 16:41:21 +0900

    0419

---
+++ src/main/java/com/example/study0419/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.study0419;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+   public static void main(String[] args) {
+       System.out.println("hello World");
+   }
+}
